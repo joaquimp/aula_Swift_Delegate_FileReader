a3deb328d587264e5820e2d18d1ff7a2dbc9028e
Joaquim Pessôa Filho <joaquimp@gmail.com>
2020-04-27 12:16:52 -0300

    Initial Commit

---
+++ aula_Swift_Delegate_FileReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aula_Swift_Delegate_FileReader
+//
+//  Created by Joaquim Pessoa Filho on 27/04/20.
+//  Copyright © 2020 Joaquim Pessoa Filho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
